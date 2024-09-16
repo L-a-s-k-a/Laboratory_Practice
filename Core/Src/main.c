e00704873799207ec11331ff64e71b82250f5c73
Vlad <vvs162@tpu.ru>
2024-09-16 19:16:45 +0700

    B7_LED_on&B14_LED

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,4 +1,38 @@
+#include "../Inc/main.h"
+
 int main(void)
 {
-    while(1){}
+    //PB7
+    // RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+    *(uint32_t*)(0x40023800UL + 0x30UL) |= 0x02;
+    
+    SET_BIT(GPIOB->MODER, GPIO_MODER_MODER7_0);
+    CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT7);
+    SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR7_0);
+    CLEAR_BIT(GPIOB->PUPDR, GPIO_PUPDR_PUPD7);
+    SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+
+    *(uint32_t*)(0x40020400UL + 0x00UL) |= 0x4000;
+    *(uint32_t*)(0x40020400UL + 0x04UL) |= 0x00;
+    *(uint32_t*)(0x40020400UL + 0x08UL) |= 0x4000;
+    *(uint32_t*)(0x40020400UL + 0x0CUL) |= 0x00;
+    *(uint32_t*)(0x40020400UL + 0x14UL) |= 0x80;
+    *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x800000;
+    //PB14
+    *(uint32_t*)(0x400207FF + 0x00) |= 0x20000000;
+    //*(uint32_t*)(0x400207FF + 0x04) |=
+    //SpeedmodeReg
+    *(uint32_t*)(0x400207FF + 0x08) |= 0x20000000;
+    //PullUpPulldownReg
+    //*(uint32_t*)(0x400207FF + 0x0C) |= ;
+    //Outputdatareg ODR
+    *(uint32_t*)(0x400207FF + 0x14) |= 0x2000;
+    //Outputdatasetresetreg BSRR
+    *(uint32_t*)(0x400207FF + 0x18) |= 0x20000000;
+    while(1)
+    {
+        *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x2000;
+        *(uint32_t*)(0x400207FF + 0x00) |= 0x2000;
+        // SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7);
+    }
 }
