48dae9337f07ad43d4b09f92c48ca675ffa19b52
xylofonsosolala <kaf13@tpu.ru>
2024-09-18 16:21:43 +0700

    ап

---
+++ Core/Src/main.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+int main(void){
+    *(uint32_t*)(0x40023800UL + 0x30UL) |= 0x02;
+
+    *(uint32_t*)(0x40020400UL + 0x00UL) |= 0x4000;
+    *(uint32_t*)(0x40020400UL + 0x08UL) |= 0x4000;
+    *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x800000;
+
+    while (1){
+        *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x80;
+    }
+}
