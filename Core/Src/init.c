814729e7c340a9ce897128dd1d527eb87562fc56
bot1ko <kirill_kodenev@mail.ru>
2024-09-24 13:56:48 +0700

    LED1_3

--- Core/Src/init.c
+++ Core/Src/init.c
@@ -8,4 +8,14 @@ void GPIO_Ini(void)
     CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT_7);        // Настройка на Push-Pull работу 7-го пина GPIOB (Output Push-Pull)
     SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR7_0); // Настройка скорости работы 7-го пина GPIOB на среднюю
     CLEAR_BIT(GPIOB->PUPDR, GPIO_PUPDR_PUPD7_0);       // Отключение PU/PD резисторов для 7-го пина GPIOB
+
+    SET_BIT(GPIOB->MODER, GPIO_MODER_MODER14_0);        // Настройка работы 14-го пина GPIOB в режиме вывода сигнала (Output mode)
+    CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT_14);        // Настройка на Push-Pull работу 14-го пина GPIOB (Output Push-Pull)
+    SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR14_0); // Настройка скорости работы 14-го пина GPIOB на среднюю
+    CLEAR_BIT(GPIOB->PUPDR, GPIO_PUPDR_PUPD14_0);       // Отключение PU/PD резисторов для 14-го пина GPIOB
+
+    SET_BIT(GPIOB->MODER, GPIO_MODER_MODER0_0);        // Настройка работы 0-го пина GPIOB в режиме вывода сигнала (Output mode)
+    CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT_0);        // Настройка на Push-Pull работу 0-го пина GPIOB (Output Push-Pull)
+    SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR0_0); // Настройка скорости работы 0-го пина GPIOB на среднюю
+    CLEAR_BIT(GPIOB->PUPDR, GPIO_PUPDR_PUPD0_0);       // Отключение PU/PD резисторов для 0-го пина GPIOB
 }
