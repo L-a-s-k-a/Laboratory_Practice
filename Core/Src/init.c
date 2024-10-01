9a98df145b696e74e232e7e8b1fd15a9acda43a1
PavelORT <rpongg@gmail.com>
2024-10-02 01:32:13 +0700

    Замена значений на макросы в файлах src

--- Core/Src/init.c
+++ Core/Src/init.c
@@ -2,9 +2,9 @@
 
 void GPIO_Ini(void)
 {
-    *(uint32_t*)(0x40023800UL + 0x30UL) |= 0x02; //Включение тактирования портов GPIOB и GPIOC
-    *(uint32_t*)(0x40020400UL + 0x00UL) |= 0x4000; //Настройка работы 7-го пина GPIOB в режиме вывода сигнала (Output mode)
-    *(uint32_t*)(0x40020400UL + 0x04UL) |= 0x00; //Настройка на Push-Pull работу 7-го пина GPIOB (Output Push-Pull)
-    *(uint32_t*)(0x40020400UL + 0x08UL) |= 0x4000; //Настройка скорости работы 7-го пина GPIOB на среднюю
-    *(uint32_t*)(0x40020400UL + 0x0CUL) |= 0x00; //Отключение PU/PD резисторов для 7-го пина GPIOB
+    RCC_GPIO_EN |= 0x02; //Включение тактирования портов GPIOB и GPIOC
+    GPIOB_MODER |= 0x4000; //Настройка работы 7-го пина GPIOB в режиме вывода сигнала (Output mode)
+    GPIOB_OTYPER |= 0x00; //Настройка на Push-Pull работу 7-го пина GPIOB (Output Push-Pull)
+    GPIOB_OSPEEDR |= 0x4000; //Настройка скорости работы 7-го пина GPIOB на среднюю
+    GPIOB_PUPDR |= 0x00; //Отключение PU/PD резисторов для 7-го пина GPIOB
 }
