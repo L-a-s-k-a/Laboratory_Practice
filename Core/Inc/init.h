aca4983c26c8b59a0e8246cf08a285f0e52a93a5
PavelORT <rpongg@gmail.com>
2024-10-02 01:48:38 +0700

    Завершение создания макросов и этапа 3

--- Core/Inc/init.h
+++ Core/Inc/init.h
@@ -9,14 +9,22 @@
 #define GPIOB_OTYPER (*(uint32_t*)(0x40020400UL + 0x04UL)) 
 #define GPIOB_OSPEEDR (*(uint32_t*)(0x40020400UL + 0x08UL)) 
 #define GPIOB_PUPDR (*(uint32_t*)(0x40020400UL + 0x0CUL))
+#define GPIOB_BSRR (*(uint32_t*)(0x40020400UL + 0x18UL))
+
+#define GPIOC_IDR (*(uint32_t*)(0x40020800UL + 0x10UL)) 
+#define GPIOC_IDR_PIN13 0x002000UL 
 
 #define GPIOB_MODE_PIN7_OUT 0x4000UL 
 #define GPIOB_OTYPE_PIN7_PP 0x0000UL 
 #define GPIOB_OSPEED_PIN7_MID 0x4000UL 
 #define GPIOB_PUPDR_PIN7_NOPUPD 0x0000UL
+#define GPIOB_BSRR_PIN7_SET 0x000080UL 
+#define GPIOB_BSRR_PIN7_RESET 0x800000UL
 
-#define READ_GPIO_C13 (*(uint32_t*)(0x40020800UL + 0x10UL) & 0x2000UL) 
-#define SET_GPIO_B7 (*(uint32_t*)(0x40020400UL + 0x18UL) |= 0x80) 
-#define RESET_GPIO_B7 (*(uint32_t*)(0x40020400UL + 0x18UL) |= 0x800000)
+#define READ_BIT(REG, BIT) (REG & BIT)
+#define SET_BIT(REG, BIT) (REG |= BIT)
+
+ 
+
 
 void GPIO_Ini(void);
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -3,11 +3,11 @@
 int main(void){
 GPIO_Ini();
 while(1){
-    if(READ_GPIO_C13 != 0){
-        SET_GPIO_B7; //Установка единицы в 7-ой бит регистра ODR
+    if(READ_BIT(GPIOC_IDR, GPIOC_IDR_PIN13 != 0)){
+        SET_BIT(GPIOB_BSRR, GPIOB_BSRR_PIN7_SET); //Установка единицы в 7-ой бит регистра ODR
         }
         else{
-        RESET_GPIO_B7; //Установка нуля в 7-ой бит регистра ODR
+        SET_BIT(GPIOB_BSRR, GPIOB_BSRR_PIN7_RESET); //Установка нуля в 7-ой бит регистра ODR
         }
     }
 }
