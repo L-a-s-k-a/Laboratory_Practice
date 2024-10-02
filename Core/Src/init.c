ebd060415b9bbf2ab7d25153e0992064b0fba97b
PavelORT <rpongg@gmail.com>
2024-10-02 23:53:32 +0700

    Лабораторная работа 1 Вариант 10 код

--- Core/Src/init.c
+++ Core/Src/init.c
@@ -5,18 +5,31 @@ void GPIO_Ini_Self(void)
 {
     //set LED 2 (pin0)
     RCC_AHB1ENR |= RCC_GPIOB_EN | RCC_GPIOC_EN; //Включение тактирования портов GPIOB и GPIOC
+    *(uint32_t*)(0x40020400UL) |= 0x10000000UL;//Настройка работы 14-го пина GPIOB в режиме вывода сигнала (Output mode)
+    *(uint32_t*)(0x40020400UL + 0x04UL) |= 0x0000UL; //Настройка на Push-Pull работу 14-го пина GPIOB (Output Push-Pull)
+    *(uint32_t*)(0x40020400UL + 0x08UL) |= 0x10000000UL; //Настройка скорости работы 14-го пина GPIOB на среднюю
+    *(uint32_t*)(0x40020400UL + 0x0CUL) |= 0x0000UL; //Отключение PU/PD резисторов для 14-го пина GPIOB
+    *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x40000000UL;//выключение светодиода 14-го пина GPIOB
+
     GPIOB_MODER |= GPIOB_MODE_PIN0_OUT; //Настройка работы 0-го пина GPIOB в режиме вывода сигнала (Output mode)
     GPIOB_OTYPER |= GPIOB_OTYPE_PIN0_PP; //Настройка на Push-Pull работу 0-го пина GPIOB (Output Push-Pull)
     GPIOB_OSPEEDR |= GPIOB_OSPEED_PIN0_MID; //Настройка скорости работы 0-го пина GPIOB на среднюю
     GPIOB_PUPDR |= GPIOB_PUPDR_PIN0_NOPUPD; //Отключение PU/PD резисторов для 0-го пина GPIOB
+    GPIOB_BSRR |= GPIOB_BSRR_PIN0_RESET;//выключение светодиода 0-го пина GPIOB
 }
 void GPIO_Ini_CMSIS(void)
 {
     SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOBEN | RCC_AHB1ENR_GPIOCEN); //Включение тактирования портов GPIOB и GPIOC
     //set LED 3 (pin7)
     SET_BIT(GPIOB->MODER, GPIO_MODER_MODE7_0); //Настройка работы 7-го пина GPIOB в режиме вывода сигнала (Output mode)
-    CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT7);//Настройка на Push-Pull работу 0-го пина GPIOB (Output Push-Pull)
+    CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT7);//Настройка на Push-Pull работу 7-го пина GPIOB (Output Push-Pull)
     SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR7_0);//Настройка скорости работы 7-го пина GPIOB на среднюю
     CLEAR_BIT(GPIOB->PUPDR, GPIO_PUPDR_PUPD7);//Отключение PU/PD резисторов для 7-го пина GPIOB
-    CLEAR_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);//сброс BSRR
+    CLEAR_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);//сброс BSRR, выключение светодиода 7-го пина GPIOB
+    //set LED 4 (pin8)
+    SET_BIT(GPIOB->MODER, GPIO_MODER_MODE8_0); //Настройка работы 8-го пина GPIOB в режиме вывода сигнала (Output mode)
+    CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT8);//Настройка на Push-Pull работу 8-го пина GPIOB (Output Push-Pull)
+    SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR8_0);//Настройка скорости работы 8-го пина GPIOB на среднюю
+    CLEAR_BIT(GPIOB->PUPDR, GPIO_PUPDR_PUPD8);//Отключение PU/PD резисторов для 8-го пина GPIOB
+    CLEAR_BIT(GPIOB->BSRR, GPIO_BSRR_BR8);//сброс BSRR, выключение светодиода 8-го пина GPIOB
 }
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -3,20 +3,68 @@
 int main(void){
     GPIO_Ini_Self();
     GPIO_Ini_CMSIS();
-    //adress to LED 1 (pin14)
+    unsigned short int counter = 0;
+    short int flag = 0;
+    //address of LED 1 (GPIOB_pin14)
+    //address of LED 2 (GPIOB_pin0)
+    //address of LED 3 (GPIOB_pin7)
+    //address of LED 4 (GPIOB_pin8)
+    //address of Button (GPIOB_pin13)
     while(1){
-        //SET_BIT(GPIOB->BSRR, GPIO_BSRR);
-        /*if(READ_BIT(GPIOC_IDR, GPIOC_IDR_PIN13) != 0){
-            SET_BIT(GPIOB_BSRR, GPIOB_BSRR_PIN7_SET); //Установка единицы в 7-ой бит регистра ODR
-            }
-            else{
-            SET_BIT(GPIOB_BSRR, GPIOB_BSRR_PIN7_RESET); //Установка нуля в 7-ой бит регистра ODR
-            }*/
         if(READ_BIT_Self(GPIOC_IDR, GPIOC_IDR_PIN13) != 0){
-            SET_BIT_Self(GPIOB_BSRR, GPIOB_BSRR_PIN0_SET); //Установка единицы в 0-ой бит регистра ODR
+            if(flag == 0){
+                flag = 1;
+                counter++;
+            }           
+        }
+        else if(flag == 1){
+            flag = 0;
+        }
+        if(counter%6 == 0)//включение всех светодиодов на 6-ое нажатие
+        {
+            *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x4000UL;//включение светодиода 14-го пина GPIOB
+            SET_BIT_Self(GPIOB_BSRR, GPIOB_BSRR_PIN0_SET); //включение светодиода 0-го пина GPIOB
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);//включение светодиода 7-го пина GPIOB
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR8);//включение светодиода 8-го пина GPIOB
+            if(counter%4 == 0)
+            {
+                counter = 0;
             }
-            else{
-            SET_BIT_Self(GPIOB_BSRR, GPIOB_BSRR_PIN0_RESET); //Установка нуля в 0-ой бит регистра ODR
-            }
+        }
+        else if (counter%5 == 0)//выключение всех светодиодов
+        {
+            *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x40000000UL;//выключение светодиода 14-го пина GPIOB
+            SET_BIT_Self(GPIOB_BSRR, GPIOB_BSRR_PIN0_RESET);//выключение светодиода 0-го пина GPIOB
+            CLEAR_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);//выключение светодиода 7-го пина GPIOB
+            CLEAR_BIT(GPIOB->BSRR, GPIO_BSRR_BR8);//выключение светодиода 8-го пина GPIOB
+        }
+        else if (counter%5 == 1)//LED 1
+        {
+            *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x4000UL;//включение светодиода 14-го пина GPIOB
+            SET_BIT_Self(GPIOB_BSRR, GPIOB_BSRR_PIN0_RESET);//выключение светодиода 0-го пина GPIOB
+            CLEAR_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);//выключение светодиода 7-го пина GPIOB
+            CLEAR_BIT(GPIOB->BSRR, GPIO_BSRR_BR8);//выключение светодиода 8-го пина GPIOB
+        }
+        else if (counter%5 == 2)//LED 2
+        {
+            *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x40000000UL;//выключение светодиода 14-го пина GPIOB
+            SET_BIT_Self(GPIOB_BSRR, GPIOB_BSRR_PIN0_SET);//включение светодиода 0-го пина GPIOB
+            CLEAR_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);//выключение светодиода 7-го пина GPIOB
+            CLEAR_BIT(GPIOB->BSRR, GPIO_BSRR_BR8);//выключение светодиода 8-го пина GPIOB
+        }
+        else if (counter%5 == 3)//LED 3
+        {
+            *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x40000000UL;//выключение светодиода 14-го пина GPIOB
+            SET_BIT_Self(GPIOB_BSRR, GPIOB_BSRR_PIN0_RESET);//выключение светодиода 0-го пина GPIOB
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);//включение светодиода 7-го пина GPIOB
+            CLEAR_BIT(GPIOB->BSRR, GPIO_BSRR_BR8);//выключение светодиода 8-го пина GPIOB
+        }
+        else if (counter%5 == 4)//LED 4
+        {
+            *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x40000000UL;//выключение светодиода 14-го пина GPIOB
+            SET_BIT_Self(GPIOB_BSRR, GPIOB_BSRR_PIN0_RESET);//выключение светодиода 0-го пина GPIOB
+            CLEAR_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);//выключение светодиода 7-го пина GPIOB
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR8);//включение светодиода 8-го пина GPIOB
+        }    
     }
 }
