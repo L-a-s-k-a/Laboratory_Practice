41c6d240902828079183656cb0db608431cbf66e
vvmanz <vvmanz23@gmail.com>
2024-10-10 21:26:42 +0700

    аав

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -15,30 +15,23 @@ int time_count;
 uint8_t current_led_position = 0;
 uint8_t all_leds_on = 1;
 
-#define GPIO_LED_MASK_B (GPIO_BSRR_BR13 | GPIO_BSRR_BR14 | GPIO_BSRR_BR15) 
-#define GPIO_LED_MASK_D (GPIO_BSRR_BR3 | GPIO_BSRR_BR4) 
+#define GPIO_LED_MASK_B (GPIO_BSRR_BR12 | GPIO_BSRR_BR13 | GPIO_BSRR_BR15) 
+#define GPIO_LED_MASK_D (GPIO_BSRR_BR3 | GPIO_BSRR_BR4| GPIO_BSRR_BR5) 
 
 void update_leds() {
-	*(uint32_t*)(0x40020400UL + 0x18UL) &= ~(1 << 5);
-	*(uint32_t*)(0x40020400UL + 0x18UL) |= (1 << 21);
     SET_BIT(GPIOB->BSRR, GPIO_LED_MASK_B);
     SET_BIT(GPIOD->BSRR, GPIO_LED_MASK_D);
     if (count == 0) {
-        *(uint32_t*)(0x40020400UL + 0x18UL) &= ~(1 << 5);
-		*(uint32_t*)(0x40020400UL + 0x18UL) |= (1 << 21);
-		SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR13 | GPIO_BSRR_BR14 | GPIO_BSRR_BR15);
-        SET_BIT(GPIOD->BSRR, GPIO_BSRR_BR3 | GPIO_BSRR_BR4);
+		SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR12 | GPIO_BSRR_BR13 | GPIO_BSRR_BR15);
+        SET_BIT(GPIOD->BSRR, GPIO_BSRR_BR3 | GPIO_BSRR_BR4 | GPIO_BSRR_BR5);
     }
     else if (count == 6) {
         if (all_leds_on) {
-            *(uint32_t*)(0x40020400UL + 0x18UL) |= (1 << 5);
-			SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS13 | GPIO_BSRR_BS14 | GPIO_BSRR_BS15);
-            SET_BIT(GPIOD->BSRR, GPIO_BSRR_BS3 | GPIO_BSRR_BS4 );
+			SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS12 | GPIO_BSRR_BS13 | GPIO_BSRR_BS15);
+            SET_BIT(GPIOD->BSRR, GPIO_BSRR_BS3 | GPIO_BSRR_BS4| GPIO_BSRR_BS5 );
         } else {
             SET_BIT(GPIOB->BSRR, GPIO_LED_MASK_B);
             SET_BIT(GPIOD->BSRR, GPIO_LED_MASK_D);
-			*(uint32_t*)(0x40020400UL + 0x18UL) &= ~(1 << 5);
-			*(uint32_t*)(0x40020400UL + 0x18UL) |= (1 << 21);
         }
     }
     else {
@@ -46,10 +39,10 @@ void update_leds() {
             uint8_t led_index = (current_led_position + i) % LED_COUNT;
             switch (led_index) {
                 case 0: SET_BIT(GPIOD->BSRR, GPIO_BSRR_BS3); break;
-                case 1: *(uint32_t*)(0x40020400UL + 0x18UL) |= (1 << 5); break;
+                case 1: SET_BIT(GPIOD->BSRR, GPIO_BSRR_BS4); break;
                 case 2: SET_BIT(GPIOD->BSRR, GPIO_BSRR_BS5); break;
-                case 3: SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS13); break;
-                case 4: SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14); break;
+                case 3: SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS12); break;
+                case 4: SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS13); break;
                 case 5: SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS15); break;
             }
         }
@@ -57,17 +50,18 @@ void update_leds() {
 }
 int main(void){
 	GPIO_Ini();
-	*(uint32_t *)(0x40020400UL + 0x00UL) |= 0x800; 
-    *(uint32_t *)(0x40020400UL + 0x04UL) |= 0x00; 
-    *(uint32_t *)(0x40020400UL + 0x08UL) |= 0x400; 
-    *(uint32_t *)(0x40020400UL + 0x0CUL) |= 0x00;
+	*(uint32_t *)(0x40020C00UL + 0x00UL) |= 0x800; // 5-ый пин в режим OUTPUT
+    *(uint32_t *)(0x40020C00UL + 0x04UL) |= 0x00; // Push-Pull
+    *(uint32_t *)(0x40020C00UL + 0x08UL) |= 0x400; // 5-ый пин MEDIUM скорость
+    *(uint32_t *)(0x40020C00UL + 0x0CUL) |= 0x00; // Отключение подтягивающих резисторов
 
     while(1){
-		current_time = time_count++ / 16000;
-		if (READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_0) == 1) {
+
+		current_time = time_count++ / 3000;
+		if (READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_8) != 0) {
 			flag_button_1 = !flag_button_1;
 		}
-		if (READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_10) != 0) {
+		if (READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_9) != 0) {
 			flag_button_2 = !flag_button_2;
 		}
 		if (flag_button_1) {
@@ -98,5 +92,7 @@ int main(void){
 				last_button_press_time_0 = current_time;
 			}
 			flag_button_2 = 0;
+		}
     }
 }
+
