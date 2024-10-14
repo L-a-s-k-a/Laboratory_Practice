377136fbb9056e5da8b72e47e414617408f94989
arskstn <ars.kstn@gmail.com>
2024-10-15 00:02:09 +0700

    Working exmaple

--- Core/Inc/init.h
+++ Core/Inc/init.h
@@ -1,7 +1,7 @@
 
 #include <stdint.h>
-#include "/home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Devices/stm32f429xx.h"
-#include "/home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Devices/stm32f4xx.h"
+//#include "/home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Devices/stm32f429xx.h"
+//#include "/home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Devices/stm32f4xx.h"
 
 
 #define RCC_GPIO_EN (*(uint32_t*)(0x40023800UL + 0x30UL))
--- build/init.d
+++ build/init.d
@@ -1,20 +1,2 @@
-build/init.o: Core/Src/init.c Core/Inc/init.h \
- /home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Devices/stm32f429xx.h \
- /home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Include/core_cm4.h \
- /home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Include/cmsis_version.h \
- /home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Include/cmsis_compiler.h \
- /home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Include/cmsis_gcc.h \
- /home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Include/mpu_armv7.h \
- /home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Devices/system_stm32f4xx.h \
- /home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Devices/stm32f4xx.h \
- /home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Devices/stm32f429xx.h
+build/init.o: Core/Src/init.c Core/Inc/init.h
 Core/Inc/init.h:
-/home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Devices/stm32f429xx.h:
-/home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Include/core_cm4.h:
-/home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Include/cmsis_version.h:
-/home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Include/cmsis_compiler.h:
-/home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Include/cmsis_gcc.h:
-/home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Include/mpu_armv7.h:
-/home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Devices/system_stm32f4xx.h:
-/home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Devices/stm32f4xx.h:
-/home/arseniikostin/Desktop/mikruhi/Laboratory_Practice/CMSIS/Devices/stm32f429xx.h:
