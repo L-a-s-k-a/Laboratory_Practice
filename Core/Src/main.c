0d9d990ad4a1015f24562531158c9430eab3320f
xylofonsosolala <kaf13@tpu.ru>
2024-10-30 14:46:48 +0700

    aaaa

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,4 +1,4 @@
-//#include <stdint.h>
+#include <stdint.h>
 #include <../Inc/init.h>
 int main(void) {
  *(uint32_t*)(0x40023800UL + 0x30UL) |= 0x02; //Включение
@@ -7,7 +7,11 @@ int main(void) {
  *(uint32_t*)(0x40020400UL + 0x08UL) |= 0x4000; //Настройка скорости
  *(uint32_t*)(0x40020400UL + 0x0CUL) |= 0x00; //Отключение PU/PD
  while(1){
-    *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x80; //Установка единицы
+    if((*(uint32_t*)(0x40020800UL + 0x10UL) & 0x2000UL) != 0){
+    *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x80; //Установка
  }
- //eee
+ else{
+ *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x800000; //Установка
+ }
+ }
 }
