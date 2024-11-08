657b6347ff8a039f6042c04d6f1665a0f4a9ea75
BourbonJr <savely.afanasenko@yandex.ru>
2024-11-09 02:15:03 +0700

    исправил прошлый недочет

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -6,10 +6,10 @@ int main(void)
     while (1)
     {
         if(READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_13)!= 0){
-            SET_BIT(GPIOb->BSRR, GPIOB_BSRR_BS7); //Установкаединицы в 7-ой бит регистра ODR
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7); //Установкаединицы в 7-ой бит регистра ODR
         }
         else{
-            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BSRR_BR7); //Установка нуля в 7-ой бит регистра ODR
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7); //Установка нуля в 7-ой бит регистра ODR
         }
     }
 }
