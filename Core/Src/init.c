957ed22b0bc160adf21713a57e11541d6594cefb
BourbonJr <savely.afanasenko@yandex.ru>
2024-11-09 20:07:57 +0700

    Осталось только понять как доделать

--- Core/Src/init.c
+++ Core/Src/init.c
@@ -1,10 +1,20 @@
 #include "init.h"
 
 void GPIO_Ini(void){
-    SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOBEN | RCC_AHB1ENR_GPIOBEN); //Включение тактирования портов GPIOB и GPIOC
+    SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOBEN | RCC_AHB1ENR_GPIOCEN); //Включение тактирования портов GPIOB и GPIOC
     
-    SET_BIT(GPIOB->MODER, GPIO_MODER_0); //Настройка работы 7-го пинаGPIOB в режиме вывода сигнала (Output mode)
-    CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT_7); //Настройка на Push-Pullработу 7-го пина GPIOB (Output Push-Pull)
-    SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR7_0); //Настройка скоростиработы 7-го пина GPIOB на среднюю
+    SET_BIT(GPIOB->MODER, GPIO_MODER_MODE7_0); //Настройка работы 7-го пина GPIOB в режиме вывода сигнала (Output mode)
+    CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT_7); //Настройка на Push-Pull работу 7-го пина GPIOB (Output Push-Pull)
+    SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR7_0); //Настройка скорости работы 7-го пина GPIOB на среднюю
     CLEAR_BIT(GPIOB->PUPDR, GPIO_PUPDR_PUPDR7_0); //Отключение PU/PDрезисторов для 7-го пина GPIOB
+
+    SET_BIT(GPIOB->MODER, GPIO_MODER_MODE14_0); //Настройка работы 14-го пина GPIOB в режиме вывода сигнала (Output mode)
+    CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT_14); //Настройка на Push-Pull работу 14-го пина GPIOB (Output Push-Pull)
+    SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR14_0); //Настройка скорости работы 14-го пина GPIOB на среднюю
+    CLEAR_BIT(GPIOB->PUPDR, GPIO_PUPDR_PUPDR14_0); //Отключение PU/PDрезисторов для 14-го пина GPIOB
+
+    SET_BIT(GPIOB->MODER, GPIO_MODER_MODE0_0); //Настройка работы 14-го пина GPIOB в режиме вывода сигнала (Output mode)
+    CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT_0); //Настройка на Push-Pull работу 14-го пина GPIOB (Output Push-Pull)
+    SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR0_0); //Настройка скорости работы 14-го пина GPIOB на среднюю
+    CLEAR_BIT(GPIOB->PUPDR, GPIO_PUPDR_PUPDR0_0); //Отключение PU/PDрезисторов для 14-го пина GPIOB
 }
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -3,13 +3,77 @@
 int main(void)
 {
     GPIO_Ini(); //Инициализация поротв GPIO
+
+    int press_butt = 0;
+    int butt_press = 0;
+
     while (1)
     {
-        if(READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_13)!= 0){
-            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7); //Установкаединицы в 7-ой бит регистра ODR
+        if(READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_13) != 0){
+            if (butt_press == 0){
+                butt_press = 1;
+                press_butt++;
+                if (press_butt > 4) {
+                    press_butt = 1;
+                }
+                
+            }
+            
         }
-        else{
-            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7); //Установка нуля в 7-ой бит регистра ODR
+        else {
+            butt_press = 0;
+        }
+        switch (press_butt)
+        {
+        case 1:
+            if (butt_press) {
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7);
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
+            } else {
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
+            }
+            break;
+        case 2:
+            if (butt_press) {
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
+            } else {
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
+            }
+            break;
+        case 3:
+            if (butt_press) {
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS0);
+            } else {
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
+            }
+            break;
+        case 4:
+            if (butt_press) {
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7);
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS0);
+            } else {
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
+            }
+            break;
+        
+        default:
+        press_butt = 0;
+            break;
+        }
+    
         }
     }
-}
