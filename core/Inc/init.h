ad1c4141f7e6f9da5340066e9a802535b83e8ee1
changyiming <976428710@qq.com>
2024-11-24 01:31:19 +0700

    lp1

--- core/Inc/init.h
+++ core/Inc/init.h
@@ -1,27 +1,57 @@
+#include<stdint.h>
 #include "../../CMSIS/Devices/stm32f4xx.h"
 #include "../../CMSIS/Devices/stm32f429xx.h"
 
 
-#define RCC_AHB1ENR                *(uint32_t *)(0x40023800UL + 0x30UL)
+#define RCC_AHB1ENR                *(uint32_t *)(0x40023800UL + 0x30UL) /*RCC地址偏移量加AHB1总线(启动GPIO时钟)地址偏移量*/
 
-#define RCC_AHB1ENR_GPIOBEN        0X02
-#define RCC_AHB1ENR_GPIOCEN        0X02
+#define RCC_AHB1ENR_GPIOBEN        0X02 /*地址偏移量*/
+#define RCC_AHB1ENR_GPIOCEN        0X04
 
-#define GPIOB_MODER                *(uint32_t *)(0x40020400UL + 0x00UL)
+#define GPIOB_MODER                *(uint32_t *)(0x40020400UL + 0x00UL)  /*GPIOB地址偏移量加MODER地址偏移量*/
 #define GPIOB_OSPEEDR              *(uint32_t *)(0x40020400UL + 0x08UL)
 #define GPIOB_BSRR                 *(uint32_t *)(0x40020400UL + 0x18UL)
 
 #define GPIO_PIN_7_OUT            0x4000UL  
 #define GPIO_PIN_7_MED            0x4000UL 
-#define GPIO_PIN_7_SET            0x80UL 
-#define GPIO_PIN_7_RESET          0x8000000UL 
+#define GPIO_PIN_7_SET            0x80UL /*蓝色led */
+#define GPIO_PIN_1_SET            0x00004000 /*红色led */
+#define GPIO_PIN_2_SET            0x00000001 /*绿色led */
+#define GPIO_PIN_7_RESET          0x800000UL 
 
-#define GPIOC_IDR                  *(uint32_t *)(0x40020800UL + 0x10UL)
+#define LED2_ON            0x80UL /*蓝色led */
+#define LED1_ON            0x00004000 /*红色led */
+#define LED3_ON            0x00000001 /*绿色led */
+#define LED1_OFF        (0x80UL << 16)     // Pb7
+#define LED2_OFF        (0x00004000UL << 16) // Pb14
+#define LED3_OFF        (0x00000001UL << 16) // Pb0
+#define BUTTON1_PIN               0x2000UL 
 
+// // 定义 LED 对应的 GPIO BSRR 位
+// #define LED1_ON (1UL << 2)  // BSRR2
+// #define LED2_ON (1UL << 6)  // BSRR6
+// #define LED3_ON (1UL << 1)  // BSRR1
+// #define LED4_ON (1UL << 4)  // BSRR4
+// #define LED5_ON (1UL << 3)  // BSRR3
+// #define LED6_ON (1UL << 5)  // BSRR5
+
+// #define LED1_OFF (1UL << 18) // BR2
+// #define LED2_OFF (1UL << 22) // BR6
+// #define LED3_OFF (1UL << 17) // BR1
+// #define LED4_OFF (1UL << 20) // BR4
+// #define LED5_OFF (1UL << 19) // BR3
+// #define LED6_OFF (1UL << 21) // BR5
+
+// // 定义按钮引脚（假设 PC13 和 PC6 分别接按钮1和按钮2）
+// #define BUTTON1_PIN (1UL << 13) // PC13
+// #define BUTTON2_PIN (1UL << 6)  // PC6
+
+#define GPIOC_IDR                  *(uint32_t *)(0x40020800UL + 0x10UL)  /*GPIOC地址偏移量加GPIOx_IDR地址偏移量*/
+#define GPIO_PIN_3               0x40UL 
 #define GPIO_PIN_13               0x2000UL 
 
 #define READ_BIT_SELF(REG,BIT) ((REG) & (BIT))
 #define SET_BIT_SELF(REG,BIT) ((REG) |= (BIT))
 
 void GPIO_Ini_Self_Def(void);
-void GPIO_Ini_Self_CMSIS(void);
+void GPIO_Ini_CMSIS(void);
--- core/Src/main.c
+++ core/Src/main.c
@@ -1,30 +1,89 @@
 #include "init.h"
 
-int main(void)
-{
-    GPIO_Ini_self_Def();
-    GPIO_Ini_CMSIS();
-    while(1)
-    {
-        if(READ_BIT_SELF(GPIOC_IDR,GPIO_PIN_13) !=0)
-        {
-            SET_BIT_SELF(GPIOB_BSRR,GPIO_PIN_7_SET);
-        }
-        else
-        {
-            SET_BIT_SELF(GPIOB_BSRR,GPIO_PIN_7_RESET);
-        }
+void delay(volatile uint32_t s) {
+    while (s--);
+}
 
+// lp1
+uint8_t current_led = 0;        // 当前点亮的 LED 起始编号（0=LED1，1=LED2，2=LED3）
+uint8_t led_count = 1;          // 每次点亮的 LED 数量（1-3）
+uint8_t button1_last_state = 1; // 按钮1上次状态
+uint8_t button2_last_state = 1; // 按钮2上次状态
 
-        if(READ_BIT(GPIOC->IDR,GPIO_IDR_ID13)!=0)
-        {
-            SET_BIT(GPIOB->BSRR,GPIO_BSRR_BS14);
-            SET_BIT(GPIOB->BSRR,GPIO_BSRR_BS0);
-        }
-        else
-        {
-            SET_BIT(GPIOB->BSRR,GPIO_BSRR_BR14);
-            SET_BIT(GPIOB->BSRR,GPIO_BSRR_BR0);
-        }
+// 更新 LED 状态函数
+void update_led_state(void) {
+    // 关闭所有 LED
+    SET_BIT_SELF(GPIOB_BSRR, LED1_OFF | LED2_OFF | LED3_OFF);
+
+    // 根据 led_count 和 current_led 点亮 LED
+    for (uint8_t i = 0; i < led_count; i++) {
+        uint8_t led_to_light = (current_led + i) % 3; // 循环点亮 LED
+        if (led_to_light == 0) SET_BIT_SELF(GPIOB_BSRR, LED1_ON);
+        if (led_to_light == 1) SET_BIT(GPIOB->BSRR,GPIO_BSRR_BS14);
+        if (led_to_light == 2) SET_BIT(GPIOB->BSRR,GPIO_BSRR_BS0);
     }
 }
+
+int main(void) {
+    // 初始化 GPIO
+    GPIO_Ini_Self_Def();
+    GPIO_Ini_CMSIS();
+
+    while (1) {
+        // 读取按钮状态
+        uint8_t button1_current_state = !(READ_BIT_SELF(GPIOC_IDR, GPIO_PIN_13) != 0);
+        uint8_t button2_current_state = !(READ_BIT(GPIOC->IDR, GPIO_IDR_ID6) != 0);
+
+        // 按钮1按下：切换当前点亮的 LED
+        if (button1_last_state == 1 && button1_current_state == 0) {
+            current_led = (current_led + led_count) % 3; // 循环切换 LED
+            update_led_state();                         // 更新 LED 状态
+        }
+        button1_last_state = button1_current_state;
+
+        // 按钮2按下：调整每次点亮的 LED 数量
+        if (button2_last_state == 1 && button2_current_state == 0) {
+            led_count = (led_count % 3) + 1; // 循环设置 LED 数量（1-3）
+            update_led_state();             // 更新 LED 状态
+        }
+        button2_last_state = button2_current_state;
+
+        // 简单防抖延迟
+        delay(100000);
+    }
+}
+
+
+
+
+
+// int main(void)
+// {
+//     GPIO_Ini_Self_Def();
+//     GPIO_Ini_CMSIS();
+
+//     while(1)
+//     {
+       
+//         if(READ_BIT_SELF(GPIOC_IDR,GPIO_PIN_13) !=0) /*使用gpioc中idr寄存器读取pc13的电平即按钮 !=不等于 */
+//         {
+//             SET_BIT_SELF(GPIOB_BSRR,GPIO_PIN_7_SET); /*使用gpiob中bsrr寄存器点亮pb7蓝色led */
+//         }
+//         else
+//         {
+//             SET_BIT_SELF(GPIOB_BSRR,GPIO_PIN_7_RESET);
+//         }
+
+
+//         if(READ_BIT(GPIOC->IDR,GPIO_IDR_ID13)!=0)  /*使用gpioc中idr寄存器读取pc13的电平即按钮（使用cmsis点亮） */
+//         {
+//             SET_BIT(GPIOB->BSRR,GPIO_BSRR_BS14); /*使用gpiob中bsrr寄存器点亮pb14红色led */
+//             SET_BIT(GPIOB->BSRR,GPIO_BSRR_BS0); /*使用gpiob中bsrr寄存器点亮pb0绿色led */
+//         }
+//         else
+//         {
+//             SET_BIT(GPIOB->BSRR,GPIO_BSRR_BR14);
+//             SET_BIT(GPIOB->BSRR,GPIO_BSRR_BR0);
+//         }
+//     }
+// }
