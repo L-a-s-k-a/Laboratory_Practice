5db290353c94c6b75f82707265b4db4857aadb15
BourbonJr <savely.afanasenko@yandex.ru>
2024-11-26 20:57:04 +0700

    Проверка на плате

--- Core/Src/init.c
+++ Core/Src/init.c
@@ -13,6 +13,21 @@ void GPIO_Ini(void){
     SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR14_0); //Настройка скорости работы 14-го пина GPIOB на среднюю
     CLEAR_BIT(GPIOB->PUPDR, GPIO_PUPDR_PUPDR14_0); //Отключение PU/PDрезисторов для 14-го пина GPIOB
 
+    SET_BIT(GPIOB->MODER, GPIO_MODER_MODE8_0); //Настройка работы 14-го пина GPIOB в режиме вывода сигнала (Output mode)
+    CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT_8); //Настройка на Push-Pull работу 14-го пина GPIOB (Output Push-Pull)
+    SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR8_0); //Настройка скорости работы 14-го пина GPIOB на среднюю
+    CLEAR_BIT(GPIOB->PUPDR, GPIO_PUPDR_PUPDR8_0); //Отключение PU/PDрезисторов для 14-го пина GPIOB
+
+    SET_BIT(GPIOB->MODER, GPIO_MODER_MODE9_0); //Настройка работы 14-го пина GPIOB в режиме вывода сигнала (Output mode)
+    CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT_9); //Настройка на Push-Pull работу 14-го пина GPIOB (Output Push-Pull)
+    SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR9_0); //Настройка скорости работы 14-го пина GPIOB на среднюю
+    CLEAR_BIT(GPIOB->PUPDR, GPIO_PUPDR_PUPDR9_0); //Отключение PU/PDрезисторов для 14-го пина GPIOB
+    
+    SET_BIT(GPIOB->MODER, GPIO_MODER_MODE10_0); //Настройка работы 14-го пина GPIOB в режиме вывода сигнала (Output mode)
+    CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT_10); //Настройка на Push-Pull работу 14-го пина GPIOB (Output Push-Pull)
+    SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR10_0); //Настройка скорости работы 14-го пина GPIOB на среднюю
+    CLEAR_BIT(GPIOB->PUPDR, GPIO_PUPDR_PUPDR10_0); //Отключение PU/PDрезисторов для 14-го пина GPIOB
+    
     SET_BIT(GPIOB->MODER, GPIO_MODER_MODE0_0); //Настройка работы 14-го пина GPIOB в режиме вывода сигнала (Output mode)
     CLEAR_BIT(GPIOB->OTYPER, GPIO_OTYPER_OT_0); //Настройка на Push-Pull работу 14-го пина GPIOB (Output Push-Pull)
     SET_BIT(GPIOB->OSPEEDR, GPIO_OSPEEDER_OSPEEDR0_0); //Настройка скорости работы 14-го пина GPIOB на среднюю
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,78 +2,72 @@
 
 int main(void)
 {
-    GPIO_Ini(); //Инициализация поротв GPIO
+    GPIO_Ini(); // Инициализация GPIO
 
-    int press_butt = 0;
-    int butt_press = 0;
+    int current_led = 0;       // Текущий активный светодиод
+    int led_count = 1;         // Количество одновременно включённых светодиодов
+    int button1_press = 0;     // Флаг для кнопки 1
+    int button2_press = 0;     // Флаг для кнопки 2
 
     while (1)
     {
-        if(READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_13) != 0){
-            if (butt_press == 0){
-                butt_press = 1;
-                press_butt++;
-                if (press_butt > 4) {
-                    press_butt = 1;
+        // ===== Обработка кнопки 1: переключение активного светодиода =====
+        if (READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_13) != 0) { // Чтение состояния кнопки 1
+            if (button1_press == 0) { // Если кнопка только что нажата
+                button1_press = 1;
+                current_led++;
+                if (current_led > 2) {
+                    current_led = 0; // Циклический переход
                 }
-                
             }
-            
+        } else {
+            button1_press = 0; // Кнопка отпущена
         }
-        else {
-            butt_press = 0;
+
+        // ===== Обработка кнопки 2: установка количества включённых светодиодов =====
+        if (READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_10) != 0) { // Чтение состояния кнопки 2
+            if (button2_press == 0) { // Если кнопка только что нажата
+                button2_press = 1;
+                led_count++;
+                if (led_count > 3) {
+                    led_count = 1; // Циклический переход
+                }
+            }
+        } else {
+            button2_press = 0; // Кнопка отпущена
         }
-        switch (press_butt)
-        {
-        case 1:
-            if (butt_press) {
-            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7);
-            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
-            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
-            } else {
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
+
+        // ===== Управление светодиодами =====
+        if (led_count == 1) {
+            // Один светодиод
+            if (current_led == 0) {
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7);  // Включить PB7
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14 | GPIO_BSRR_BR0); // Выключить остальные
+            } else if (current_led == 1) {
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);  // Включить PB14
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7 | GPIO_BSRR_BR0);
+            } else if (current_led == 2) {
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS0); // Включить PB0
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7 | GPIO_BSRR_BR14);
             }
-            break;
-        case 2:
-            if (butt_press) {
-            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
-            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);
-            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
-            } else {
+        } else if (led_count == 2) {
+            // Два светодиода
+            if (current_led == 0) {
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7 | GPIO_BSRR_BS14); // Включить PB7 и PB14
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0); // Выключить остальные
+            } else if (current_led == 1) {
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14 | GPIO_BSRR_BS0); // Включить PB14 и PB0
                 SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+            } else if (current_led == 2) {
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS0 | GPIO_BSRR_BS7); // Включить PB0 и PB7
                 SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
             }
-            break;
-        case 3:
-            if (butt_press) {
-            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
-            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
-            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS0);
-            } else {
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
-            }
-            break;
-        case 4:
-            if (butt_press) {
-            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7);
-            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);
-            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS0);
-            } else {
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
-            }
-            break;
-        
-        default:
-        press_butt = 0;
-            break;
+        } else if (led_count == 3) {
+            // Три светодиода
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7 | GPIO_BSRR_BS14 | GPIO_BSRR_BS0); // Включить все
         }
-    
-        }
+
+        // ===== Задержка для подавления дребезга =====
+        for (volatile int i = 0; i < 100000; i++) {}
     }
+}
