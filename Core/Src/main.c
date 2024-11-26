42dee56a25fd78842af7bcc75843139d77e18b8f
BourbonJr <savely.afanasenko@yandex.ru>
2024-11-26 21:04:31 +0700

    1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,68 +2,78 @@
 
 int main(void)
 {
-    GPIO_Ini();
+    GPIO_Ini(); //Инициализация поротв GPIO
 
-    int current_led = 0;       
-    int led_count = 1;         
-    int button1_press = 0;     
-    int button2_press = 0; 
+    int press_butt = 0;
+    int butt_press = 0;
 
     while (1)
     {
-        if (READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_13) != 0) {
-            if (button1_press == 0) {
-                button1_press = 1;
-                current_led++;
-                if (current_led > 2) {
-                    current_led = 0;
+        if(READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_13) != 0){
+            if (butt_press == 0){
+                butt_press = 1;
+                press_butt++;
+                if (press_butt > 4) {
+                    press_butt = 1;
                 }
+                
             }
-        } else {
-            button1_press = 0;
+            
         }
-
-        if (READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_10) != 0) {
-            if (button2_press == 0) {
-                button2_press = 1;
-                led_count++;
-                if (led_count > 3) {
-                    led_count = 1;
-                }
-            }
-        } else {
-            button2_press = 0;
+        else {
+            butt_press = 0;
         }
-
-
-        if (led_count == 1) {
-            if (current_led == 0) {
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7);
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14 | GPIO_BSRR_BR0);
-            } else if (current_led == 1) {
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7 | GPIO_BSRR_BR0);
-            } else if (current_led == 2) {
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS0);
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7 | GPIO_BSRR_BR14);
-            }
-        } else if (led_count == 2) {
-
-            if (current_led == 0) {
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7 | GPIO_BSRR_BS14);
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
-            } else if (current_led == 1) {
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14 | GPIO_BSRR_BS0);
+        switch (press_butt)
+        {
+        case 1:
+            if (butt_press) {
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7);
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
+            } else {
                 SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
-            } else if (current_led == 2) {
-                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS0 | GPIO_BSRR_BS7);
                 SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
             }
-        } else if (led_count == 3) {
-            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7 | GPIO_BSRR_BS14 | GPIO_BSRR_BS0);
+            break;
+        case 2:
+            if (butt_press) {
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
+            } else {
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
+            }
+            break;
+        case 3:
+            if (butt_press) {
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS0);
+            } else {
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
+            }
+            break;
+        case 4:
+            if (butt_press) {
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7);
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS0);
+            } else {
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
+                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
+            }
+            break;
+        
+        default:
+        press_butt = 0;
+            break;
         }
-
-
-        for (volatile int i = 0; i < 100000; i++) {}
+    
+        }
     }
-}
