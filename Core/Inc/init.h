9acb71fe9b9f7aedd54a0c36db626fd220b72f63
Moiseev Nikita <149692118+MoiseevNS@users.noreply.github.com>
2024-12-03 12:54:24 +0700

    Add files via upload

--- Core/Inc/init.h
+++ Core/Inc/init.h
@@ -1,6 +1,19 @@
 #include "C:\Users\mns94\OneDrive\Documents\digital_microprocessor_technology\Laboratory_Practice-main\CMSIS\Devices\stm32f4xx.h"
 #include "C:\Users\mns94\OneDrive\Documents\digital_microprocessor_technology\Laboratory_Practice-main\CMSIS\Devices\stm32f429xx.h"
 
+#define GPIOB_MODER (*(uint32_t*)(0x40020400UL + 0x00UL))
+#define GPIOB_OTYPER (*(uint32_t*)(0x40020400UL + 0x04UL)) 
+#define GPIOB_OSPEEDR (*(uint32_t*)(0x40020400UL + 0x08UL)) 
+#define GPIOB_PUPDR (*(uint32_t*)(0x40020400UL + 0x0CUL))
+#define GPIOB_BSRR (*(uint32_t*)(0x40020400UL + 0x18UL))
+
+#define GPIOB_MODE_PIN0_OUT 0x0001UL 
+#define GPIOB_OTYPE_PIN0_PP 0x0000UL 
+#define GPIOB_OSPEED_PIN0_MID 0x0001UL 
+#define GPIOB_PUPDR_PIN0_NOPUPD 0x0000UL
+#define GPIOB_BSRR_PIN0_SET 0x000001UL 
+#define GPIOB_BSRR_PIN0_RESET 0x010000UL
+
 void GPIO_Ini(void);
 void GPIO_Ini_Self_Def(void);
 void GPIO_Ini_CMSIS(void);
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,50 +1,103 @@
 #include <init.h>
 #include <stdint.h>
 
-int button_state=0;
-int led_state=0;
-int led_num=0;
+int button_state_1=0;
+int button_state_2=0;
+int button1_clicks=0;
+int button2_clicks=0;
+int alternative=0;
+int port_C0_state=0; // 0 - out;   1 - in
 
-void check_button_state(){
-        if(button_state==0 && READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_13)!=0){
-                button_state=1;  
-                led_num+=1;
-                for(int i=0;i<500000;i++);
+void check_alternative()
+{
+        if(alternative==0 && button2_clicks%6==5){
+                alternative=1;
+                button2_clicks=0;
         }
-        if(button_state==1 && READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_13)!=0){
-                button_state=0;  
-                led_num+=1;
-                for(int i=0;i<500000;i++);
+        if(alternative==1 && button2_clicks%6==5){
+                alternative=0;
+                button2_clicks=0;
         }
 }
+void check_button_state_1()
+{
+        if(alternative==0 && button_state_1==0 && READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_0)!=0){
+                button_state_1=1;  
+                button1_clicks+=1;
+                for(int i=0;i<750000;i++);
+        }
+        if(button_state_1==1 && READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_0)!=0){
+                button_state_1=0;  
+                button1_clicks+=1;
+                for(int i=0;i<750000;i++);
+        }
+}
+void check_button_state_2()
+{
+        if(button_state_2==0 && READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_13)!=0){
+                button_state_2=1;  
+                button2_clicks+=1;
+                for(int i=0;i<750000;i++);
+        }
+        if(button_state_2==1 && READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_13)!=0){
+                button_state_2=0;  
+                button2_clicks+=1;
+                for(int i=0;i<750000;i++);
+        }
+}
+void reconfigure_pin_C0_to_in(){
+        CLEAR_BIT(GPIOC->MODER, GPIO_MODER_MODE0_0);
+        CLEAR_BIT(GPIOC->OTYPER, GPIO_OTYPER_OT_0);
+        CLEAR_BIT(GPIOC->OSPEEDR, GPIO_OSPEEDER_OSPEEDR0_0);
+        CLEAR_BIT(GPIOC->PUPDR, GPIO_PUPDR_PUPDR0_0);
+}
+void reconfigure_pin_C0_to_out(){
+        SET_BIT(GPIOC->MODER, GPIO_MODER_MODE0_0);
+        CLEAR_BIT(GPIOC->OTYPER, GPIO_OTYPER_OT_0);
+        SET_BIT(GPIOC->OSPEEDR, GPIO_OSPEEDER_OSPEEDR0_0);
+        CLEAR_BIT(GPIOC->PUPDR, GPIO_PUPDR_PUPDR0_0);
+}
 
-int main(void){
+int main(void)
+{
         GPIO_Ini();
         GPIO_Ini_Self_Def();
         GPIO_Ini_CMSIS();
         while(1){
-                check_button_state();
-                if(led_num==1)SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS0);
-                if(led_num==2)SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7);
-                if(led_num==3)SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);
-                if(led_num==4)SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS8);
-                
-                if(led_num==5){
-                        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
-                        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
-                        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
-                        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR8);
-                        led_num=0;
+                check_alternative();
+                if(alternative==0){
+                        if(port_C0_state==0){
+                                reconfigure_pin_C0_to_in();
+                                port_C0_state=1;
+                        }
+                        check_button_state_1();
+                        check_button_state_2();
+                        if(button1_clicks%4==1)SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS0);
+                        if(button1_clicks%4==2)SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7);
+                        if(button1_clicks%4==3)SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);
+                        if(button1_clicks%4==0){
+                                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
+                                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+                                SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
+                        }
+                }
+                if(alternative==1){
+                        if(port_C0_state==1){
+                                reconfigure_pin_C0_to_out();
+                                port_C0_state=0;
+                        }
+                        check_button_state_2();
+                        if(button2_clicks%2==1)SET_BIT(GPIOC->BSRR, GPIO_BSRR_BS0);
+                        if(button2_clicks%2==0)SET_BIT(GPIOC->BSRR, GPIO_BSRR_BR0);
+
                 }
         }
 
         // *(uint32_t*)(0x40023800UL + 0x30UL) |= 0x02;
-        // adsas
         // *(uint32_t*)(0x40020400UL + 0x00UL) |= 0x4000;
         // *(uint32_t*)(0x40020400UL + 0x08UL) |= 0x4000;
         // *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x800000;
         
-
         // while(1){
         //         *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x80;
         // }
