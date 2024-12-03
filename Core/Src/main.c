e79f7eee0524ed62baa9f0d1124aaf948d8379a2
Moiseev Nikita <149692118+MoiseevNS@users.noreply.github.com>
2024-12-03 12:56:08 +0700

    Add files via upload

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -92,20 +92,4 @@ int main(void)
 
                 }
         }
-
-        // *(uint32_t*)(0x40023800UL + 0x30UL) |= 0x02;
-        // *(uint32_t*)(0x40020400UL + 0x00UL) |= 0x4000;
-        // *(uint32_t*)(0x40020400UL + 0x08UL) |= 0x4000;
-        // *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x800000;
-        
-        // while(1){
-        //         *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x80;
-        // }
-        // while(1){
-        //         if(READ_BIT(GPIOC_IDR, GPIOC_IDR_PIN13) != 0){
-        //                 SET_BIT(GPIOB_BSRR, GPIOB_BSRR_PIN7_SET);
-        //         }else{
-        //                 SET_BIT(GPIOB_BSRR, GPIOB_BSTT_PIN7_RESET);
-        //         }    
-        // }
 }
