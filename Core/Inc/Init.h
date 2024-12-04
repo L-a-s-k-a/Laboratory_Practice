00f5e8a960707f61b96093ffee1271e4a9c6b779
Shtefan <darvinosi2012@gmail.com>
2024-12-04 18:44:35 +0700

    Final_changes

--- Core/Inc/Init.h
+++ Core/Inc/Init.h
@@ -3,36 +3,6 @@
 #include "../../CMSIS/Devices/stm32f429xx.h"
 #include "../../CMSIS/Devices/stm32f4xx.h"
 
-
-// #define RCC_GPIO_EN (*(uint32_t*)(0x40023800UL + 0x30UL)) // Тактирование
-
-// #define RCC_GPIOB_EN 0x02UL
-// #define RCC_GPIOC_EN 0x04UL
-
-// #define GPIOB_MODER (*(uint32_t*)(0x40020400UL + 0x00UL)) // Ввод-вывод
-// #define GPIOB_OTYPER (*(uint32_t*)(0x40020400UL + 0x04UL)) // PUSH/PULL
-// #define GPIOB_OSPEEDER (*(uint32_t*)(0x40020400UL + 0x08UL)) //скорость работы
-// #define GPIOB_PUPDR (*(uint32_t*)(0x40020400UL + 0x0CUL)) // 
-// #define GPIOB_BSRR (*(uint32_t*)(0x40020400UL + 0x18UL))
-
-// #define GPIOC_IDR  (*(uint32_t*)(0x40020800UL + 0x10UL))
-
-// #define GPIOB_MODE_PIN7_OUT 0x4000UL
-// #define GPIOB_OTYPE_PIN7_PP 0x0000UL
-// #define GPIOB_OSPEED_PIN7_MID 0x4000UL
-// #define GPIOB_PUPDR_PIN7_NOPUPD 0x0000UL
-// #define GPIOB_BSRR_PIN7_SET 0x000080UL
-// #define GPIOB_BSRR_PIN7_RESET 0x800000UL
-
-// #define GPIOC_IDR_PIN13 0x002000UL
-
-// #define READ_BIT(REG, BIT) (REG & BIT)
-// #define SET_BIT(REG, BIT) (REG |= BIT)
-
-// #define READ_GPIO_C13 (*(uint32_t*)(0x40020800UL + 0x10UL) & 0x2000UL)
-// #define SET_GPIO_B7 (*(uint32_t*)(0x40020400UL + 0x18UL) |= 0x80)
-// #define RESET_GPIO_B7 (*(uint32_t*)(0x40020400UL + 0x18UL) |= 0x800000)
-
 void GPIO_Init(void);
 void GPIO_led_init(void);
 void GPIO_button_output(void);
@@ -43,23 +13,3 @@ void button3_output(void);
 void button1_input(void);
 void button2_input(void);
 void button3_input(void);
-// #define GPIOB_RCC_ON *(uint32_t *)(0x40023800UL + 0x30UL) |= 0x02; // включение тактирования GPIOB
-// #define GPIOC_RCC_ON *(uint32_t *)(0x40023800UL + 0x30UL) |= 0x04; // включение тактирования GPIOC 
-// #define PIN7_OUT *(uint32_t *)(0x40020400UL + 0x00UL) |= 0x4000;   // 7пин на вывод
-// #define PIN7_PUSH_PULL *(uint32_t *)(0x40020400UL + 0x04UL) |= 0x00;     // 7пин push-pull
-// #define PIN7_SPEED *(uint32_t *)(0x40020400UL + 0x08UL) |= 0x4000;   // 7пин средняя скорость
-
-// #define READ_PIN_C13 ((*(uint32_t *)(0x40020800UL + 0x10UL) & 0x2000))
-// #define PIN7_SET *(uint32_t *)(0x40020400UL + 0x18UL) |= 0x80; // 7пин - 1 (0DR)
-// #define PIN7_RESET *(uint32_t *)(0x40020400UL + 0x18UL) |= 0x800000; // 7пин - 0 (0DR)
-
-
-// #define RCC_GPIOB_EN 0x02UL
-// #define RCC_GPIOC_EN 0x04UL
-
-// #define GPIOB_MODE_PIN7_OUT 0x4000UL
-// #define GPIOB_OTYPE_PIN7_PP 0x0000UL
-// #define GPIOB_OSSPEED_PIN7_MID 0x4000UL
-// #define GPIOB_PUPDR_PIN7_NOPUPD 0x0000UL
-
-// void GPIO_Ini(void);
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,97 +1,59 @@
 #include <stdint.h>
 #include "Init.h"
 
-uint8_t mode, mode_prev = 0; // 10000000 & (1 << 7)
+uint8_t mode, mode_prev = 0;
 uint8_t count = 4;
-// uint8_t state = 0b00000111;//0 - output / 1 - input
-/*  
-state (uint8_t):
-    1   0   0   0   0   0   0   0
-    b1  b2  b3  l1  l2  l3
-*/
+uint8_t b1 = 0;
+uint8_t b2 = 2;
+uint8_t b3 = 4;
+
 int main(void){
-    // GPIOB_RCC_ON;
-    // GPIOC_RCC_ON;
-    //  *(uint32_t *)(0x40023800UL + 0x30UL) |= 0x02; // включение тактирования GPIOB
-    //  *(uint32_t *)(0x40023800UL + 0x30UL) |= 0x04; // включение тактирования GPIOC
-    //  *(uint32_t *)(0x40020400UL + 0x00UL) |= 0x4000;   // 7пин на вывод
-    //  *(uint32_t *)(0x40020400UL + 0x04UL) |= 0x00;     // 7пин push-pull
-    //  *(uint32_t *)(0x40020400UL + 0x08UL) |= 0x4000;   // 7пин средняя скорость
-    //  *(uint32_t *)(0x40020400UL + 0x18UL) |= 0x800000; // 7пин - 0 (0DR)
-    GPIO_Init();
     GPIO_led_init();
     GPIO_button_input();
-    //GPIO_button_output();
     while (1)
     {
-        // if (((*(uint32_t *)(0x40020800UL + 0x10UL) & 0x2000)) != 0){
-        //     *(uint32_t *)(0x40020400UL + 0x18UL) |= 0x80; // 7пин - 1 (0DR)
-        // }
-        // else *(uint32_t *)(0x40020400UL + 0x18UL) |= 0x800000; // 7пин - 0 (0DR)
-        // if(READ_BIT(GPIOC_IDR, GPIOC_IDR_PIN13) != 0){
-        //     SET_BIT(GPIOB_BSRR, GPIOB_BSRR_PIN7_SET); //Установка 1 в 7 бит регистра
-        // }
-        // else{
-        //     SET_BIT(GPIOB_BSRR, GPIOB_BSRR_PIN7_RESET); //Установка 0 в 7 бит регистра
-        // }
-        // if(READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_13) != 0){
-        //     flag = 1;
-        //     SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7); //Установка 1 в 7 бит регистра
-        // }
-        // else{
-        //     flag = 0;
-        //     SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7); //Установка 0 в 7 бит регистра
-        // }
-        // SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS8); //Установка 1 в 7 бит регистра
-        // if(READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_8) !=  0){
-        //     flag = 1;
-        //     SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7); //Установка 1 в 7 бит регистра
-        // }
-        // else{
-        //     flag = 0;
-        //     SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7); //Установка 0 в 7 бит регистра
-        // }
-
-        //SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
-        mode = (READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_13) != 0);
-        if (mode && mode_prev){
+      
+        mode = (READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_3) != 0);
+        if (mode == 1 && mode_prev == 0){
             count += 1;
             switch (count) {
-            case 1: button1_input(); break;
-            case 2: button2_input(); break;
-            case 3: button3_input(); break;
-            case 4: GPIO_button_input(); break;
-            case 5: GPIO_button_output(); break;
-            case 6: count = 1; break;
+            case 1: button1_output(); break;
+            case 2: button2_output(); break;
+            case 3: button3_output(); break;
+            case 4: GPIO_button_output(); break;
+            case 5: GPIO_button_input(); 
+                    count = 0; break;
             }
         }
         mode_prev = mode;
-
-    // if (READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_12) == 0 && but1 == 1)
-    // {
-    //     SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS8);
-    // }
-    // else
-    // {
-    //     SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR8);
-    // }
-    // if (READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_15) == 0 && but2 == 1)
-    // {
-    //     SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS5);
-    // }
-    // else
-    // {
-    //     SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR5);
-    // }
-    // if (READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_9) == 0 && but3 == 1)
-    // {
-    //     SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS6);
-    // }
-    // else
-    // {
-    //     SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR6);
-    // }
-    
-}
+        //b1-pb14
+        if(!READ_BIT(GPIOB->MODER, GPIO_MODER_MODE12_0) && READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_12) != 0){ 
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);
+            b1 = 1;
+        }
+        else {
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14); //set pb12 
+            b1 = 0;
+        }
+        //b2-pb7
+        if(!READ_BIT(GPIOB->MODER, GPIO_MODER_MODE15_0) && READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_15) != 0){ 
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7);
+            b2 = 3;
+            }
+        else{
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7); //set pb15
+            b2 = 2;
+        }
+        //b3-pb0
+        if(!(*(uint32_t*)(0x40020400UL + 0x00UL) | 0b10) && (((*(uint32_t *)(0x40020800UL + 0x10UL) & 0x2000)) != 0) != 0){
+              *(uint32_t *)(0x40020400UL + 0x18UL) |= 1<<0;
+              b3 = 5;
+        }
+        else {
+            *(uint32_t *)(0x40020400UL + 0x18UL) |= 1<<16; //set pb0
+            b3 = 4;
+        }
+             
+}               
 
 }
