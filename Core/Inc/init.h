7d92736f8f3be62c80aa0231891a5b8876991870
Valya1200 <lyalyala6174@mail.ru>
2024-12-17 22:24:27 +0700

    LomakaV_8E21_LB1_variant3

--- Core/Inc/init.h
+++ Core/Inc/init.h
@@ -1,27 +1,3 @@
-// #include <stdint.h>
 #include "../../CMSIS/Devices/stm32f4xx.h"
 #include "../../CMSIS/Devices/stm32f429xx.h"
-
-#define  RCC_AAB1ENR *(uint32_t *)(0x40023800UL + 0x30UL)
-
-#define  RCC_GPIOBEN 0x02
-#define  RCC_GPIOCEN 0x04
-
-#define  GPIO_MODER    *(uint32_t *)(0x40020400UL + 0x00UL)
-#define  GPIO_OSPEEDR  *(uint32_t *)(0x40020400UL + 0x08UL)
-#define  GPIO_BSRR     *(uint32_t *)(0x40020400UL + 0x18UL)
-
-#define  GPIO_PIN_7_OUT 0x4000UL
-#define  GPIO_PIN_7_MED 0x4000UL
-#define  GPIO_PIN_7_SET 0x80UL
-#define  GPIO_PIN_7_RESET 0x800000UL
-
-#define  GPIO_IDR *(uint32_t *)(0x40020800UL + 0x10UL)
-
-#define  GPIO_PIN_13 0x2000UL
-
-#define  READ_BIT_SELF(REG,BIT) ((REG)&(BIT))
-#define  SET_BIT_SELF(REG,BIT) ((REG)|=(BIT))
-
-void GPIO_Ini_Self_Def(void);
-void GPIO_Ini_CMSIS(void);
+void GPIO_Init_CMSIS(void);
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,30 +1,70 @@
-// #include <stdint.h>
-#include "init.h"
+#include <../Inc/init.h>
+uint8_t button1=0, button2=0, button3=0;
+int button1_clicks=0;
+int counter=0;
+int flag=0;
 
-int main(void){
-    GPIO_Ini_Self_Def();
-    GPIO_Ini_CMSIS();
-    // *(uint32_t*)(0x40023800UL + 0x30UL) |= 0x02;
+int main(void){  
 
-    // *(uint32_t*)(0x40020400UL + 0x00UL) |= 0x4000;
-    // *(uint32_t*)(0x40020400UL + 0x08UL) |= 0x4000;
-    // *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x800000;
-
-    while(1){
-        if(READ_BIT_SELF(GPIO_IDR, GPIO_PIN_13)!=0){
-            SET_BIT_SELF (GPIO_BSRR,GPIO_PIN_7_SET);
-        }
-        else{
-            SET_BIT_SELF(GPIO_BSRR, GPIO_PIN_7_RESET);
-
-        }
-
-        if(READ_BIT(GPIOA->IDR, GPIO_IDR_ID9)!=0){
-        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);
-
-        }
-
-        else{
-        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);
-    }}
+GPIO_Init_CMSIS();
+while (1){
+   if(READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_13)!=0 && flag==0) {
+      flag=1;
+      for(int i=0;i<750000;i++);
+      if( counter==0){
+         CLEAR_BIT(GPIOB->MODER, GPIO_MODER_MODE0_Msk);
+         SET_BIT(GPIOB->MODER, GPIO_MODER_MODE12_0);
+         SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS12);
+         counter+=1;
+      }
+      else if(counter==1){
+         CLEAR_BIT(GPIOB->MODER, GPIO_MODER_MODE7_Msk);
+         SET_BIT(GPIOB->MODER, GPIO_MODER_MODE11_0);
+         SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS11);
+         counter+=1;
+      }
+      else if(counter==2){
+         CLEAR_BIT(GPIOB->MODER, GPIO_MODER_MODE14_Msk);
+         SET_BIT(GPIOB->MODER, GPIO_MODER_MODE10_0);
+         SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS10);
+         counter+=1;
+      }
+      else if(counter==3){
+         SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR12);
+         SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR11);
+         SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR10);
+        counter=0;
+      }}
+   if(READ_BIT(GPIOC->IDR, GPIO_IDR_IDR_13)==0 ) {
+      flag=0;
+   }
+   if(counter==0){ 
+      SET_BIT(GPIOB->MODER, GPIO_MODER_MODE0_0);
+      SET_BIT(GPIOB->MODER, GPIO_MODER_MODE7_0);
+      SET_BIT(GPIOB->MODER, GPIO_MODER_MODE14_0);
+      CLEAR_BIT(GPIOB->MODER, GPIO_MODER_MODE12_Msk);
+      CLEAR_BIT(GPIOB->MODER, GPIO_MODER_MODE11_Msk);
+      CLEAR_BIT(GPIOB->MODER, GPIO_MODER_MODE10_Msk);
+   }
+     
+   if(READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_12)!=0 ){
+      SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS0);
+   }
+   if(READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_12)==0){
+      SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
+   }
+   if(READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_11)!=0 ){
+      SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7);
+   }
+   if(READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_11)==0){
+      SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+   }
+   if(READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_10)!=0 ){
+      SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS14);
+   }
+   if(READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_10)==0){
+      SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR14);
+   }
+   }
 }
+
