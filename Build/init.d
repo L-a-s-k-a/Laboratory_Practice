dc884e682f2fd07ab5b6781f3fef1952e95d06ea
ld01 <ld01@tpu.ru>
2024-12-19 02:14:10 +0700

    end

--- Build/init.d
+++ Build/init.d
@@ -1,20 +1,20 @@
-Build/init.o: Core/Src/init.c Core/Src/../Inc/init.h \
- Core/Src/../Inc/../../CMSIS/Devices/stm32f4xx.h \
- Core/Src/../Inc/../../CMSIS/Devices/stm32f429xx.h \
+Build/init.o: Core/Src/init.c Core/Inc/init.h \
+ Core/Inc/../../CMSIS/Devices/stm32f4xx.h \
+ Core/Inc/../../CMSIS/Devices/stm32f429xx.h \
  D:/Laboratory_Practice/CMSIS/Include/core_cm4.h \
  D:/Laboratory_Practice/CMSIS/Include/cmsis_version.h \
  D:/Laboratory_Practice/CMSIS/Include/cmsis_compiler.h \
  D:/Laboratory_Practice/CMSIS/Include/cmsis_gcc.h \
  D:/Laboratory_Practice/CMSIS/Include/mpu_armv7.h \
- Core/Src/../Inc/../../CMSIS/Devices/system_stm32f4xx.h \
- Core/Src/../Inc/../../CMSIS/Devices/stm32f429xx.h
-Core/Src/../Inc/init.h:
-Core/Src/../Inc/../../CMSIS/Devices/stm32f4xx.h:
-Core/Src/../Inc/../../CMSIS/Devices/stm32f429xx.h:
+ Core/Inc/../../CMSIS/Devices/system_stm32f4xx.h \
+ Core/Inc/../../CMSIS/Devices/stm32f429xx.h
+Core/Inc/init.h:
+Core/Inc/../../CMSIS/Devices/stm32f4xx.h:
+Core/Inc/../../CMSIS/Devices/stm32f429xx.h:
 D:/Laboratory_Practice/CMSIS/Include/core_cm4.h:
 D:/Laboratory_Practice/CMSIS/Include/cmsis_version.h:
 D:/Laboratory_Practice/CMSIS/Include/cmsis_compiler.h:
 D:/Laboratory_Practice/CMSIS/Include/cmsis_gcc.h:
 D:/Laboratory_Practice/CMSIS/Include/mpu_armv7.h:
-Core/Src/../Inc/../../CMSIS/Devices/system_stm32f4xx.h:
-Core/Src/../Inc/../../CMSIS/Devices/stm32f429xx.h:
+Core/Inc/../../CMSIS/Devices/system_stm32f4xx.h:
+Core/Inc/../../CMSIS/Devices/stm32f429xx.h:
