be21888c70c4ceb0f205b6da19fc531f2d8640d8
SunYifan <SunneYi@outlook.com>
2024-12-28 18:10:38 +0700

    Sunyifan1228

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,15 +1,18 @@
 #include "init.h"
 
 #define DEBOUNCE_DELAY 20000 // 防抖延迟
+#define LIGHT_DELAY 500000   // 每个LED点亮后的延迟时间
 
 void delay(uint32_t s) {
     while (s--) {
         __NOP(); // 空循环
     }
 }
-uint8_t flag1=0;
-uint8_t flag2=0;
-uint8_t flag3=0;
+
+uint8_t flag1 = 0;
+uint8_t flag2 = 0;
+uint8_t flag3 = 0;
+
 int main(void) {
     GPIO_Ini(); // 初始化 GPIO
     uint8_t leds[] = {BOARD_LED1_PIN, BOARD_LED2_PIN, BOARD_LED3_PIN, CN8_LED4_PIN, CN8_LED5_PIN, CN8_LED6_PIN};
@@ -19,23 +22,24 @@ int main(void) {
     uint8_t button2_function = 0; // 按钮2功能（熄灭）
 
     while (1) {
-        flag1=(READ_BIT(GPIOC_IDR, (1 << BUTTON1_PIN)) != 0);
-        flag2=(READ_BIT(GPIOC_IDR, (1 << BUTTON2_PIN)) != 0);
-        flag3=(READ_BIT(GPIOC_IDR, (1 << BUTTON3_PIN)) != 0);
+        flag1 = (READ_BIT(GPIOC_IDR, (1 << BUTTON1_PIN)) != 0);
+        flag2 = (READ_BIT(GPIOC_IDR, (1 << BUTTON2_PIN)) != 0);
+        flag3 = (READ_BIT(GPIOC_IDR, (1 << BUTTON3_PIN)) != 0);
+
         // 检测按钮1
-        if ((READ_BIT(GPIOC_IDR, (1 << BUTTON1_PIN)) != 0)) {
+        if (flag1) {
             delay(DEBOUNCE_DELAY);
-            if (READ_BIT(GPIOC_IDR, (1 << BUTTON1_PIN)) != 0) {
+            if (flag1) {
                 if (button1_function) { // 点亮模式
                     if (led_state < 6) {
-                         delay(DEBOUNCE_DELAY);
                         SET_BIT(GPIOB_BSRR, (1 << leds[led_state])); // 点亮 LED
+                        delay(LIGHT_DELAY); // 添加延迟以体现逐个点亮
                         led_state++;
                     }
                 } else { // 熄灭模式
                     if (led_state > 0) {
                         led_state--;
-                         delay(DEBOUNCE_DELAY);
+                        delay(LIGHT_DELAY); // 添加延迟以体现逐个熄灭
                         SET_BIT(GPIOB_BSRR, (1 << (leds[led_state] + 16))); // 熄灭 LED
                     }
                 }
@@ -43,19 +47,19 @@ int main(void) {
         }
 
         // 检测按钮2
-        if ((READ_BIT(GPIOC_IDR, (1 << BUTTON2_PIN)) != 0)) {
+        if (flag2) {
             delay(DEBOUNCE_DELAY);
-            if (READ_BIT(GPIOC_IDR, (1 << BUTTON2_PIN)) != 0) {
+            if (flag2) {
                 if (button2_function) { // 点亮模式
                     if (led_state < 6) {
-                         delay(DEBOUNCE_DELAY);
                         SET_BIT(GPIOB_BSRR, (1 << leds[led_state])); // 点亮 LED
+                        delay(LIGHT_DELAY); // 添加延迟以体现逐个点亮
                         led_state++;
                     }
                 } else { // 熄灭模式
                     if (led_state > 0) {
                         led_state--;
-                         delay(DEBOUNCE_DELAY);
+                        delay(LIGHT_DELAY); // 添加延迟以体现逐个熄灭
                         SET_BIT(GPIOB_BSRR, (1 << (leds[led_state] + 16))); // 熄灭 LED
                     }
                 }
@@ -63,15 +67,13 @@ int main(void) {
         }
 
         // 检测按钮3
-        if ((READ_BIT(GPIOC_IDR, (1 << BUTTON3_PIN)) != 0)) {
+        if (flag3) {
             delay(DEBOUNCE_DELAY);
-            if (READ_BIT(GPIOC_IDR, (1 << BUTTON3_PIN)) != 0) {
+            if (flag3) {
                 // 交换功能
                 button1_function ^= 1;
                 button2_function ^= 1;
             }
         }
-
-        // delay(20000); // 延时，防止循环过快
     }
 }
