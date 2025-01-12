5f534048241d19ceb82dc46d9fd6b30be1af79f7
SunYifan <SunneYi@outlook.com>
2025-01-12 09:15:21 +0800

    SunYifan8EM41_10.20Lab1

--- Build/init.d
+++ Build/init.d
@@ -1,19 +1,20 @@
 Build/init.o: Core/Src/init.c Core/Inc/init.h \
- CMSIS/Devices/Inc/stm32f4xx.h CMSIS/Devices/Inc/stm32f429xx.h \
+ Core/Inc/../../CMSIS/Devices/Inc/stm32f4xx.h \
+ Core/Inc/../../CMSIS/Devices/Inc/stm32f429xx.h \
  CMSIS/Include/../include/core_cm4.h \
  CMSIS/Include/../include/cmsis_version.h \
  CMSIS/Include/../include/cmsis_compiler.h \
  CMSIS/Include/../include/cmsis_gcc.h \
  CMSIS/Include/../include/mpu_armv7.h \
- CMSIS/Devices/Inc/system_stm32f4xx.h \
- CMSIS/Devices/Inc/system_stm32f4xx.h
+ Core/Inc/../../CMSIS/Devices/Inc/system_stm32f4xx.h \
+ Core/Inc/../../CMSIS/Devices/Inc/stm32f429xx.h
 Core/Inc/init.h:
-CMSIS/Devices/Inc/stm32f4xx.h:
-CMSIS/Devices/Inc/stm32f429xx.h:
+Core/Inc/../../CMSIS/Devices/Inc/stm32f4xx.h:
+Core/Inc/../../CMSIS/Devices/Inc/stm32f429xx.h:
 CMSIS/Include/../include/core_cm4.h:
 CMSIS/Include/../include/cmsis_version.h:
 CMSIS/Include/../include/cmsis_compiler.h:
 CMSIS/Include/../include/cmsis_gcc.h:
 CMSIS/Include/../include/mpu_armv7.h:
-CMSIS/Devices/Inc/system_stm32f4xx.h:
-CMSIS/Devices/Inc/system_stm32f4xx.h:
+Core/Inc/../../CMSIS/Devices/Inc/system_stm32f4xx.h:
+Core/Inc/../../CMSIS/Devices/Inc/stm32f429xx.h:
