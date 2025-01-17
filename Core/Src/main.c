e19a133a0182ac4897d92eda8956d7e22919a516
arskstn <ars.kstn@gmail.com>
2025-01-17 12:03:53 +0300

    finished lab1.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,32 +1,55 @@
 #include "init.h"
 
-int main(void) {
-    GPIO_Init();
+// Глобальные переменные для мониторинга
+volatile uint8_t num_leds = 1;        // Количество одновременно включаемых светодиодов
+volatile uint8_t button1_state = 0;   // Состояние кнопки D8 (0 = не нажата, 1 = нажата)
+volatile uint8_t button2_state = 0;   // Состояние кнопки D9 (0 = не нажата, 1 = нажата)
+volatile uint8_t current_led = 0;     // Индекс текущего включенного светодиода
+volatile uint8_t leds_on = 0;         // Состояние светодиодов (0 = выкл, 1 = вкл)
 
-    uint8_t num_leds = 1;
-    uint8_t leds_on = 0;
+int main(void) {
+    GPIO_Init(); // Инициализация GPIO
 
     while (1) {
-
-        if (GPIOF_IDR & GPIOF_PIN12) {
+        // Проверка состояния кнопки D8 (PF12)
+        button1_state = (GPIOF_IDR & GPIOF_PIN12) ? 1 : 0;
+        if (button1_state) {
             num_leds = (num_leds % 6) + 1;
             while (GPIOF_IDR & GPIOF_PIN12);
         }
 
-        if (GPIOD_IDR & GPIOD_PIN15) {
-            if (leds_on) {
+        button2_state = (GPIOD_IDR & GPIOD_PIN15) ? 1 : 0;
+        if (button2_state) {
+            if (num_leds == 6) {
+                if (leds_on) {
+                    GPIOG_BSRR = (GPIOG_PIN9 | GPIOG_PIN14) << 16;
+                    GPIOF_BSRR = (GPIOF_PIN15 | GPIOF_PIN14) << 16;
+                    GPIOE_BSRR = (GPIOE_PIN13 | GPIOE_PIN11) << 16;
+                    leds_on = 0;
+                } else {
+                    GPIOG_BSRR = GPIOG_PIN9 | GPIOG_PIN14;
+                    GPIOF_BSRR = GPIOF_PIN15 | GPIOF_PIN14;
+                    GPIOE_BSRR = GPIOE_PIN13 | GPIOE_PIN11;
+                    leds_on = 1;
+                }
+            } else {
                 GPIOG_BSRR = (GPIOG_PIN9 | GPIOG_PIN14) << 16;
                 GPIOF_BSRR = (GPIOF_PIN15 | GPIOF_PIN14) << 16;
                 GPIOE_BSRR = (GPIOE_PIN13 | GPIOE_PIN11) << 16;
-                leds_on = 0;
-            } else {
-                if (num_leds >= 1) GPIOG_BSRR = GPIOG_PIN9;  // D0
-                if (num_leds >= 2) GPIOG_BSRR = GPIOG_PIN14; // D1
-                if (num_leds >= 3) GPIOF_BSRR = GPIOF_PIN15; // D2
-                if (num_leds >= 4) GPIOE_BSRR = GPIOE_PIN13; // D3
-                if (num_leds >= 5) GPIOF_BSRR = GPIOF_PIN14; // D4
-                if (num_leds >= 6) GPIOE_BSRR = GPIOE_PIN11; // D5
-                leds_on = 1;
+
+                for (uint8_t i = 0; i < num_leds; i++) {
+                    uint8_t led_idx = (current_led + i) % 6;
+                    switch (led_idx) {
+                        case 0: GPIOG_BSRR = GPIOG_PIN9; break;
+                        case 1: GPIOG_BSRR = GPIOG_PIN14; break;
+                        case 2: GPIOF_BSRR = GPIOF_PIN15; break;
+                        case 3: GPIOE_BSRR = GPIOE_PIN13; break;
+                        case 4: GPIOF_BSRR = GPIOF_PIN14; break;
+                        case 5: GPIOE_BSRR = GPIOE_PIN11; break;
+                    }
+                }
+
+                current_led = (current_led + 1) % 6;
             }
             while (GPIOD_IDR & GPIOD_PIN15);
         }
